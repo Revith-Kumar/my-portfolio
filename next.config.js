ebcbf26ffe2e8484ec279b432fc579b81bc58195
Revith kumar <157215286+Revith-Kumar@users.noreply.github.com>
2025-06-27 22:34:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  images:{
-    unoptimized:true,
-  }
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverActions: true, // Optional: only if you're using Server Actions
+  },
+};
+
 module.exports = nextConfig;
